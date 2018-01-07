993b5026f927b0410a0a1286cf83cdc0d71589f5
Alex Shpak <alex-shpak@users.noreply.github.com>
2018-01-07 17:54:21 +0100

    Fixed domain line generation for certbot (v2)

--- certbot-domains.py
+++ certbot-domains.py
@@ -2,6 +2,6 @@ import os
 
 # Converts HTTPS_DOMAINS env variable into list of domain params for certbot
 domains = os.environ['HTTPS_DOMAINS'].split()
-print ' \\ \n'.join(
+print ' '.join(
   ['-d %s' % domain for domain in domains]
 )
